30b507326314236471e5a01fc92bf18a4693e84f
catalin572 <clatcovschi@gmail.com>
2022-11-06 12:04:42 +0200

    add books component

--- src/app/client/books/books.module.ts
+++ src/app/client/books/books.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
+import { BooksComponent } from './books.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BooksComponent
+  ],
   imports: [
     CommonModule,
     BooksRoutingModule
